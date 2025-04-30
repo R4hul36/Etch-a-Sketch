c24a08e57df33dcfcb48aa103621063e55a079ce
Rahul <rahul.wa47@gmail.com>
2025-04-30 23:36:01 +0530

    Created a grid of tiles and added hover effect

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('Hello world')
+ 
+let squareTiles = 16 * 16 ;
+const containerWidth = 700;
+let tilewidth = 700/16;
+
+for(let i =0; i<squareTiles; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add(".tile");
+    tile.setAttribute("style", `width: ${tilewidth}px; height:${tilewidth}px;`)
+    const tileContainer = document.querySelector(".container");
+    tileContainer.appendChild(tile);
+
+    //event listener
+    tile.addEventListener("mouseover", (e) => {
+        tile.style.backgroundColor = 'lightblue';
+    })
+}
+
+
+
+
