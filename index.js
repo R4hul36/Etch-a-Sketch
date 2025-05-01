59a775578c15dfd4226b5585d1721703d59aa760
Rahul <rahul.wa47@gmail.com>
2025-05-01 15:35:50 +0530

    added functionality to the button that changes the square count

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 console.log('Hello world')
- 
-let squareTiles = 16 * 16 ;
-const containerWidth = 600;
-let tilewidth = 600/16;
 
-for(let i =0; i<squareTiles; i++) {
-    const tile = document.createElement("div");
-    tile.classList.add(".tile");
-    tile.setAttribute("style", `width: ${tilewidth}px; height:${tilewidth}px;`)
-    const tileContainer = document.querySelector(".container");
-    tileContainer.appendChild(tile);
+const createGrid = function (gridSize = 16) {
+  const containerWidth = 600
+  const tileContainer = document.querySelector('.container')
+  let tilewidth = 600 / gridSize
+  gridSize = gridSize ** 2
+  // removing all the tiles when the function is called again
+  tileContainer.replaceChildren()
+  for (let i = 0; i < gridSize; i++) {
+    const tile = document.createElement('div')
+    tile.classList.add('.tile')
+    tile.setAttribute('style', `width: ${tilewidth}px; height:${tilewidth}px;`)
+
+    tileContainer.appendChild(tile)
 
     //event listener
-    tile.addEventListener("mouseover", (e) => {
-        tile.style.backgroundColor = 'lightblue';
+    tile.addEventListener('mouseover', (e) => {
+      tile.style.backgroundColor = 'lightblue'
     })
+  }
 }
 
 // change grid size
 
-const changeButton = document.querySelector("#change-size");
+const changeButton = document.querySelector('#change-size')
 
-changeButton.addEventListener("click", (e) => {
-    let size;
-    do{
-        size = prompt("Enter a number between 1 and 100");
-    }
-    while(size > 100 || size < 2)
+changeButton.addEventListener('click', (e) => {
+  let size
+  do {
+    size = prompt('Enter a number between 1 and 100')
+  } while (size > 100 || size < 2)
+  createGrid(size)
 })
 
-
+createGrid()
