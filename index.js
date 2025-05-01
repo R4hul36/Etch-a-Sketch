87849cc4ea523d5ad26ec23499a7e5b34c3baa05
Rahul <rahul.wa47@gmail.com>
2025-05-01 12:27:14 +0530

    Added a button to change grid count

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('Hello world')
  
 let squareTiles = 16 * 16 ;
-const containerWidth = 700;
-let tilewidth = 700/16;
+const containerWidth = 600;
+let tilewidth = 600/16;
 
 for(let i =0; i<squareTiles; i++) {
     const tile = document.createElement("div");
@@ -17,6 +17,16 @@ for(let i =0; i<squareTiles; i++) {
     })
 }
 
+// change grid size
 
+const changeButton = document.querySelector("#change-size");
+
+changeButton.addEventListener("click", (e) => {
+    let size;
+    do{
+        size = prompt("Enter a number between 1 and 100");
+    }
+    while(size > 100 || size < 2)
+})
 
 
